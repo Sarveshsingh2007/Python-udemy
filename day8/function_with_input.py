f41febc70247ebbc47bc737ce253b6ba550d2802
Sarvesh singh <sarveshadhikari905@gmail.com>
2025-06-29 11:58:36 +0530

    function parameters

---
+++ day8/function_with_input.py
@@ -0,0 +1,13 @@
+# def greet():
+#     print("Hello, World!")
+#     print("This side Sarvesh")
+#     print("Do you want my friendship!")
+    
+# greet()    
+
+# Functions that allows for inputs
+
+def greet_with_name(name): # name - parameter - name of the data that been passed in. 
+    print(f"Hello, {name}")
+
+greet_with_name("Sarvesh")    # sarvesh - argument - it is the actual value of the data 
