3eff01db7a4f5e614100469a2e69868b24992e8c
Sarvesh singh <sarveshadhikari905@gmail.com>
2025-06-29 10:54:21 +0530

    datatypes
    Understanding datatypes and how to manipulate strings

---
+++ day2/datatypes.py
@@ -0,0 +1,19 @@
+# Subscripting
+print("hello"[4]) 
+print("sarvesh"[-1])
+
+#string
+print("123" + "345") #concatenation
+
+#integer = whole number
+print(123 + 345)
+
+#Large Intergers
+print(345_342_524)
+
+#float = floating point number 
+print(3141.59)
+
+#boolean
+print(True)
+print(False)
