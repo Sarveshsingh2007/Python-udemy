c562768e728c3905cb39c321d3c931d262052722
Sarvesh singh <sarveshadhikari905@gmail.com>
2025-06-29 11:27:13 +0530

    function in python
    understanding function concept

---
+++ day6/function.py
@@ -0,0 +1,12 @@
+# print("hello")
+
+# num_char = len("hello")
+# print(num_char)  #len function
+
+# own function creating
+
+def my_function(): # defining the function
+    print("hello")
+    print("bye")
+
+my_function()      # calling the function
