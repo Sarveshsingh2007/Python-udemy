e09b9261ceadce7250b4d07bfc7ea037a7dc080a
Sarvesh singh <sarveshadhikari905@gmail.com>
2025-07-02 16:53:05 +0530

    Create global_constant

---
+++ day12/global_constant.py
@@ -0,0 +1,12 @@
+# GLOBAL CONSTANT = A global constant is a variable defined outside all functions (globally) and is meant to stay unchanged throughout the program.
+# Global constant is written in UPPERCASE by convention to show it should not be modified.
+PI = 3.14159
+
+def my_function():
+     print(PI)
+
+# my_function()
+
+def area_of_circle(radius):
+     print(PI * radius * radius)
+area_of_circle(2)
