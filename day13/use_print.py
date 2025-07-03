0a7440abd8ac3f8a4885902b3c2facfc37a3cbd3
Sarvesh singh <sarveshadhikari905@gmail.com>
2025-07-03 18:27:00 +0530

    Create use_print.py
    use print statement for finding the bug in program

---
+++ day13/use_print.py
@@ -0,0 +1,16 @@
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+
+# print(f"Pages : {pages}")
+# print(f"Words per page: {word_per_page}")
+# print(total_words)
+
+
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+
+print(total_words)
