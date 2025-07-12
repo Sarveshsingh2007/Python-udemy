fab40bc32daf95e9ebb43671ecde17a0ce679c07
Sarvesh singh <sarveshadhikari905@gmail.com>
2025-07-12 13:36:35 +0530

    Create import_modules.py
    imported modules , installed packages and used Aliases

---
+++ day18/import_modules.py
@@ -0,0 +1,16 @@
+# import turtle
+# tim = turtle.Turtle()
+
+# from turtle import Turtle
+# tim = Turtle()
+# tom = Turtle()
+# Jerry = Turtle()
+ 
+# WORKING WITH ALIASES
+
+# import turtle as t   
+# tim = t.Turtle()
+
+import heroes
+print(heroes.gen())
+import villains
