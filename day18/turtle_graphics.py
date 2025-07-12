b663e5b814081032e3533b224544d1e0d8407303
Sarvesh singh <sarveshadhikari905@gmail.com>
2025-07-12 13:38:28 +0530

    Create turtle_graphics.py

---
+++ day18/turtle_graphics.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+timmy.fd(100)
+timmy.right(90)
+timmy.fd(50)
+
+
+
+screen = Screen()
+screen.exitonclick()
