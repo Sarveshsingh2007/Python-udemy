431e8cdd8a6c2ec64049e51784fe11103e4079df
Sarvesh singh <sarveshadhikari905@gmail.com>
2025-07-20 09:25:23 +0530

    Update main.py
    here i have updated the missing states using list comprehension to make shorter code

--- day25/India State Game/main.py
+++ day25/India State Game/main.py
@@ -18,10 +18,11 @@ while len(guessed_states) < 30:
                                      prompt="What's another state's name?").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
+        # missing_states = []
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)   
         new_data.to_csv(r"udemy\day25\India_states_game\states_to_learn.csv")     
         break
