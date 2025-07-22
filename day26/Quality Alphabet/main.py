3e513d7b3c71ba120f3fa742cb6e9570d1215470
Sarvesh singh <sarveshadhikari905@gmail.com>
2025-07-22 12:39:46 +0530

    Updated main.py using exception handling

--- day26/Quality Alphabet/main.py
+++ day26/Quality Alphabet/main.py
@@ -3,6 +3,13 @@ import pandas
 data = pandas.read_csv(r"udemy\day26\Quality_alphabet\qualities_alphabet.csv")
 quality_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
-output_list = [quality_dict[letter] for letter in word]
-print(output_list)
+def generate_quality():
+  word = input("Enter a word: ").upper()
+  try:
+    output_list = [quality_dict[letter] for letter in word]
+  except KeyError:
+    print("Sorry, only letters in the alphabet please.")
+    generate_quality()
+  else:
+    print(output_list)
+generate_quality()
