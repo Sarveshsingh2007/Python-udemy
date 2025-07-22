44a8dd83fb50ae12171ced7d040e7d29c1d78a60
Sarvesh singh <sarveshadhikari905@gmail.com>
2025-07-22 14:14:08 +0530

    Update password_manager.py
    here i have saved password in json file and added search button for finding the password of user's website using exception handling.

--- day29/password_manager.py
+++ day29/password_manager.py
@@ -2,80 +2,114 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
-# Password generator
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+#Password Generator Project
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '@', '#', '$', '%', '^', '&', '*']
-
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
     password_letters = [choice(letters) for _ in range(randint(8, 10))]
     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
     password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
-    password_list = password_letters + password_numbers + password_symbols
+    password_list = password_letters + password_symbols + password_numbers
     shuffle(password_list)
 
     password = "".join(password_list)
     password_entry.insert(0, password)
     pyperclip.copy(password)
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
 
     website = website_entry.get()
-    email = mail_entry.get()
+    email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
-        messagebox.showinfo(title="opps",message="Please don't leave any fields empty!")
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}" 
-                                                    f"\nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open(r"udemy\day29\data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open(r"udemy\day29\data.json", "r") as data_file:
+                #Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open(r"udemy\day29\data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            #Updating old data with new data
+            data.update(new_data)
+
+            with open(r"udemy\day29\data.json", "w") as data_file:
+                #Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
 
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("udemy\day29\data.json") as data_files:
+            data = json.load(data_files)
+    except:
+        messagebox.showinfo(title="Error", message="No Data file founds")
+    else:            
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+
+# ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
-canvas = Canvas(width=200, height=200)
-logo_img = PhotoImage(file=r"udemy\day29\logo.png")
-canvas.create_image(100, 100, image= logo_img)
-canvas.grid(column=1, row=0)
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="udemy\day29\logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
 
-# LABELS
+#Labels
 website_label = Label(text="Website:")
-website_label.grid(column=0, row=1)
-
+website_label.grid(row=1, column=0)
 email_label = Label(text="Email/Username:")
-email_label.grid(column=0, row=2)
-
+email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
-password_label.grid(column=0, row=3)
+password_label.grid(row=3, column=0)
 
-# ENTRIES
-website_entry = Entry(width=52)
-website_entry.grid(row=1,column=1, columnspan=2)
+#Entries
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
+email_entry = Entry(width=39)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "sarveshsingh9381gmail.com")
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
 
-mail_entry = Entry(width=52)
-mail_entry.grid(row=2,column=1, columnspan=2)
-mail_entry.insert(0, "sarveshsingh9381@gmail.com")
-password_entry = Entry(width=34)
-password_entry.grid(column=1, row=3)
-
-
-# BUTTONS
-pass_gen = Button(text="Generate Password", command=generate_password)
-pass_gen.grid(column=2, row=3)
-
-add = Button(text="Add", width=44, command=save)
-add.grid(row=4, column=1, columnspan=2)
+# Buttons
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
+generate_password_button = Button(text="Generate Password", command=generate_password)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=33, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
