f59ce362b2559358015cd05035e3e8e6447bf146
Sarvesh singh <sarveshadhikari905@gmail.com>
2025-07-31 20:48:25 +0530

    Create habit_tracker.py

---
+++ day37/habit_tracker.py
@@ -0,0 +1,66 @@
+import requests
+from datetime import datetime
+
+USERNAME = "adhikari"
+TOKEN = "sarvesh123securetokenadhikari"
+GRAPH_ID = "cycle1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_parameters = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService":"yes",
+    "notMinor": "yes"
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_parameters)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_parameters = {
+    "id": GRAPH_ID,
+    "name": "Cycling Graph",
+    "unit": "km",
+    "type": "float",
+    "color": "momiji"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# graph_response = requests.post(url=graph_endpoint, json=graph_parameters, headers=headers)
+# print(graph_response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+# print(today)
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many kilometers did you cycle today? ")
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+value_response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(value_response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "4.5"
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+response = requests.delete(url=delete_endpoint, headers=headers)
+print(response.text)
