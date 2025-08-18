455a29fc791f61d7910e1869af091cdfc34ad81c
Sarvesh singh <sarveshadhikari905@gmail.com>
2025-08-18 11:18:31 +0530

    Create advance_decorator1.py

---
+++ day55/advance_decorator1.py
@@ -0,0 +1,16 @@
+# TODO: Create the logging_decorator() function 
+
+def logging_decorator(func):
+    def wrapper(*args):
+        print(f"You called {func.__name__}{args}")
+        result = func(*args)
+        print(f"It returned: {result}")
+    return wrapper
+
+# TODO: Use the decorator
+
+@logging_decorator
+def a_function(*args):
+    return sum(args)
+
+a_function(1,2,3)    
